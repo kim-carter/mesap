adb7a01517bce7dd073426af537bea9b415595cb
kim-carter <kimwarwickcarter@gmail.com>
2020-06-14 00:39:54 +0800

    Updated paths to key programs - as they are now system-wide installed

--- pipelines/localprograms.groovy
+++ pipelines/localprograms.groovy
@@ -1,6 +1,8 @@
 BASEROOTDIR="/mesap"
-HISAT2 = "/usr/bin/hisat2"
-STRINGTIE = "/usr/bin/stringtie"
-SAMTOOLS = "/usr/bin/samtools"
+// these are in system-wide path
+HISAT2 = "hisat2"
+STRINGTIE = "stringtie"
+SAMTOOLS = "samtools"
+// these are in mesap dir only
 CHECKBAM = "$BASEROOTDIR"+ "/scripts/check_bam_files.sh"
 GLUE_4_BALLGOWN = "$BASEROOTDIR"+ "/pipelines/make_ballgown_obj.sh"

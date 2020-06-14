2241fbe549aa5c28d47a21e376bc6d4b70339883
kim-carter <kimwarwickcarter@gmail.com>
2020-06-14 23:44:47 +0800

    Added samstat, multiqc, and fastqc to programs list

--- pipelines/localprograms.groovy
+++ pipelines/localprograms.groovy
@@ -3,6 +3,11 @@ BASEROOTDIR="/mesap"
 HISAT2 = "hisat2"
 STRINGTIE = "stringtie"
 SAMTOOLS = "samtools"
+SAMSTAT = "samstat"
+MULTIQC = "multiqc"
+
 // these are in mesap dir only
 CHECKBAM = "$BASEROOTDIR"+ "/scripts/check_bam_files.sh"
 GLUE_4_BALLGOWN = "$BASEROOTDIR"+ "/pipelines/make_ballgown_obj.sh"
+FASTQC = "$BASEROOTDIR"+"/programs/FastQC/fastqc"
+
